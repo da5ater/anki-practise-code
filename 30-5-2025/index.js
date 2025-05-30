922e71206d4b2bbd39b9f3888bcf292806a52bad
Mohamed <107060533+da5ater@users.noreply.github.com>
2025-05-30 17:31:17 +0300

    added pagination

--- 30-5-2025/index.js
+++ 30-5-2025/index.js
@@ -1,22 +1,31 @@
-const http = require('http');
+const mongoose = require('mongoose');
+const { default: test } = require('node:test');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello, World!\n');
-    } else if (req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('About Page\n');
-    }
-});
+mongoose.connect('mongodb://localhost:27017/mydatabase')
 
-server.on('connection', (socket) => {
-    console.log('New connection established');
-    socket.on('close', () => {
-        console.log('Connection closed');
-    });
-});
+const courseSchema = new mongoose.Schema({
+    name: String,
+    tags: [String],
+    date: { type: Date, default: Date.now }
+})
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-});
+const Course = mongoose.model('Course', courseSchema)
+
+async function getCourses(n) {
+    let pageNumber = n || 1;
+    let pageSize = 5;
+    const courses = await Course.find()
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+
+    console.log(courses);
+};
+
+async function testPagination() {
+    await getCourses(1);
+    console.log('-------------------');
+    await getCourses(2);
+    console.log('-------------------');
+    await getCourses(3);
+}
+testPagination();
