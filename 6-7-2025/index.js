0332e729dfd6b47888012f3ca4cb2c04bd8f4fc0
Mohamed <107060533+da5ater@users.noreply.github.com>
2025-06-07 16:22:13 +0300

    today is done

--- 6-7-2025/index.js
+++ 6-7-2025/index.js
@@ -1,7 +1,20 @@
-const bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+const path = require('path');
+const pug = require('pug');
+const config = require('config');
 
-function hashPassword(password) {
-    const saltRounds = bcrypt.genSaltSync(10);
-    return bcrypt.hash(password, saltRounds);
-}
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
+
+console.log(`pass is ${config.get('password')}`);
+
+app.get('/', (req, res) => {
+    res.render('temp', { title: 'Home', message: 'Welcome to the Home Page!', date: new Date().toLocaleDateString() });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
