e7cb5b4f6ab978f35056ce911263d9e75fdac1d6
Mohamed <107060533+da5ater@users.noreply.github.com>
2025-06-08 15:21:38 +0300

    seventh card

--- 6-8-2025/index.js
+++ 6-8-2025/index.js
@@ -1,60 +1,35 @@
-const express = require('express');
-const app = express();
-const debugserver = require('debug')('app:server');
-const debugdb = require('debug')('app:db');
-const mongoose = require('mongoose');
-
-
-
-mongoose.connect('mongodb://localhost:27017/mydatabase')
-
-    .then(() => console.log('MongoDB connected...'))
-
-    .catch(err => console.log(err));
-
-
-
-const courseSchema = new mongoose.Schema({
+const authorSchema = new mongoose.Schema({
 
     name: String,
 
-    author: String,
+    bio: String,
 
-    tags: [String],
-
-    date: { type: Date, default: Date.now },
-
-    isPublished: Boolean,
-
-    price: Number,
-    age: { type: Number, min: 18, max: 65, required: true }
+    website: String
 
 });
 
 
 
-const Course = mongoose.model('Course', courseSchema);
+const Author = mongoose.model('Author', authorSchema);
 
 
-async function getCourses() {
-    const courses = await Course.find({ age : {$in: [18, 20, 22] } });
-    return courses;
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+
+    name: String,
+
+    author: {
+
+        type: authorSchema,
+
+        required: true
+
+    }
+
+}));
+
+
+async function updateAuthor(courseId, authorData) {
+    const course = await Course.updateOne(        { _id: courseId },
+        { $set: { author: authorData } });
 }
-
-
-
-// //server logic
-// debugserver('Starting server...');
-
-
-// //database logic 
-// debugdb('Connecting to database...');
-
-app.get('/api/posts/:year/:month', (req, res) => {
-    const { year, month } = req.params;
-    res.send(`Posts for ${month}/${year}`);
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
